291c544a4247d337c5f31c1144ded31c9e17abf9
Bharat Kumar <149879338+Bharat-Kumar-19030@users.noreply.github.com>
2026-04-30 21:52:11 +0530

    Time: 421 ms (5.01%), Space: 47.9 MB (14.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,49 +1,51 @@
 class Solution {
-    boolean contain(HashMap<Character,Integer> set,HashMap<Character,Integer> map){
-        for(Map.Entry<Character,Integer> entry:set.entrySet()){
-            if(!map.containsKey(entry.getKey())){
-                return false;
-            }else if(map.containsKey(entry.getKey())&&map.get(entry.getKey())<entry.getValue()){
-                return false;
-            }
+    boolean eq(HashMap<Character,Integer> map,HashMap<Character,Integer> map2){
+        for(Map.Entry<Character,Integer> ent:map.entrySet()){
+            if(!map2.containsKey(ent.getKey())||map2.get(ent.getKey())<ent.getValue())return false;
         }
         return true;
     }
     public String minWindow(String s, String t) {
-        int ptr1 = 0, ptr2 = 0;
-        HashMap<Character,Integer> set = new HashMap<>();
-        int setlen=0,maplen=0;
-        for (int i = 0; i < t.length(); i++) {
-            set.put(t.charAt(i),set.getOrDefault(t.charAt(i), 0) + 1);
-            setlen++;
+        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
         }
-        HashMap<Character, Integer> map = new HashMap<>();
-        String ret = "";
-        while (ptr2 < s.length()) {
-            char curr = s.charAt(ptr2);
-            ptr2++;
-            if (set.containsKey(curr)) {
-                map.put(curr, map.getOrDefault(curr, 0) + 1);
-                maplen++;
-            }
-            if(map.size()>0){
-                while (!map.containsKey(s.charAt(ptr1))
-                        || (map.containsKey(s.charAt(ptr1)) && map.get(s.charAt(ptr1)) > set.get(s.charAt(ptr1)))) {
-                    
-                    if (map.containsKey(s.charAt(ptr1))) {
-                        map.put(s.charAt(ptr1), (map.get(s.charAt(ptr1))) - 1);
-                        maplen--;
-                    }
-                    ptr1++;
+        int st=0;
+        String ret="";
+        int ans=s.length();
+        for(int i=0;i<s.length();i++){            
+            // if(map.containsKey(s.charAt(i))){
+                map2.put(s.charAt(i),map2.getOrDefault(s.charAt(i),0)+1);
+                // System.out.println("At: "+i);
+            // }
+            // System.out.println(map+" "+map2);
+            while(st<=i&&eq(map,map2)){
+                
+                // System.out.println(st+" "+i);
+                if(ans>=i-st+1){
+                    ans=i-st+1;
+                    ret=s.substring(st,i+1);
                 }
+                // if(map2.containsKey(s.charAt(st))){
+                    map2.put(s.charAt(st),map2.getOrDefault(s.charAt(st),0)-1);
+                    if(map2.get(s.charAt(st))<=0){
+                        map2.remove(s.charAt(st));
+                    }
+                // }                
+                st++;
             }
             
-            if (contain(set,map) && (ret.equals("") || ret.length() > ptr2 - ptr1)) {
-                ret = s.substring(ptr1, ptr2);
-            }
-
-
         }
+        // if(eq(map,map2)){
+        //     System.out.println(map+" "+map2);
+        //     // System.out.println(st+" "+i);
+        //     if(ans>=s.length()-st+1){
+        //         ans=s.length()-st+1;
+        //         ret=s.substring(st);
+        //     }
+        // }
         return ret;
+
     }
 }
