21e6084ae67460c3ac18063e192998e96ec18021
Bharat Kumar <149879338+Bharat-Kumar-19030@users.noreply.github.com>
2026-05-15 09:45:47 +0530

    Time: 0 ms (100%), Space: 43.4 MB (96.58%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    int ans=Integer.MAX_VALUE;
+    void check(int nums[],int l,int h){
+        if(l>h)return;
+        int mid=l+(h-l)/2;
+        if(nums[mid]>=nums[l]){
+            ans=Math.min(ans,nums[l]);
+            check(nums,mid+1,h);
+        }
+        else{
+            ans=Math.min(ans,nums[mid]);
+            check(nums,l,mid-1);
+        }
+    }
+    public int findMin(int[] nums) {
+        if(nums[0]<nums[nums.length-1])return nums[0];
+        check(nums,0,nums.length-1);
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
